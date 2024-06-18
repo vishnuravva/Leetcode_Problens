b142df90e82f97fbf13c6efadac3b37ac088e1f5
Vishnu Ravva <96168813+vishnuravva@users.noreply.github.com>
2024-06-18 21:05:57 +0530

    Update Plus_One.java

--- Plus_One.java
+++ Plus_One.java
@@ -1,15 +1,32 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i=digits.length - 1;i >= 0 ;i--){
-              if(digits[i] < 9){
-                 digits[i]++;
-                 return digits;
-              }      
-              digits[i] = 0;
-        }
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
-        return digits;
 
+        // first soln
+        // for(int i=digits.length - 1;i >= 0 ;i--){
+        //       if(digits[i] < 9){
+        //          digits[i]++;
+        //          return digits;
+        //       }      
+        //       digits[i] = 0;
+        // }
+        // digits = new int[digits.length + 1];
+        // digits[0] = 1;
+        // return digits;
+
+// Second soln
+     int i = digits.length - 1;
+     while(i >= 0){
+            if(digits[i] == 9){
+                digits[i] = 0;
+                i--;
+            }
+            else{
+ digits[i] += 1;
+            return digits;
+            }
+     }
+     digits = new int[digits.length + 1];
+     digits[0] = 1;
+     return digits;
     }
 }
