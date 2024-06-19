e5f5a8b8f87d26f0d50fedc0a2cbb9c6b5c88886
Vishnu Ravva <96168813+vishnuravva@users.noreply.github.com>
2024-06-20 00:36:14 +0530

    Sqrt.java soln

--- Sqrt.java
+++ Sqrt.java
@@ -1,1 +1,34 @@
 
+class Solution {
+    public int mySqrt(int n) {
+    // Using binary search
+        int start = 0;
+        int end = n;
+        int mid = -1;
+         
+        while(start <= end){
+            mid = (start + end) / 2;
+            
+            if(mid * mid == n) return mid;
+            else if((long) mid * mid > (long)n) {
+                end = mid - 1; 
+            }
+            
+            else {
+                start = mid + 1;
+            }
+        }
+        return Math.round(end);
+
+// brute force approach
+        // int i = 1;
+        // while(i <= n/i){
+        //     if(n/i == i){
+        //         return i;
+        //     }
+        //     i++;
+           
+        // }
+        //  return i-1;
+    }
+}
