05194cf0b9d42168250da7775cb9f6f3c0491cb3
Vishnu Ravva <96168813+vishnuravva@users.noreply.github.com>
2024-06-22 00:04:00 +0530

    add binary soln added

--- AddBinary.java
+++ AddBinary.java
@@ -1,5 +1,27 @@
 class Solution {
     public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0){
+                sum += a.charAt(i) - '0';
+            }
+            if(j >= 0){
+                sum += b.charAt(j) - '0';
+            }
+            result.append(sum % 2);
+            carry = sum / 2;
+i--;
+j--;
+           
+        }
+        if(carry != 0){
+            result.append(carry);
+        }
+ return result.reverse().toString();
     }
 }
